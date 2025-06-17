a7bc38414adea6e944a82105833e5e0c2dbec70d
MuhamadIqbalFaturachman <ifaturahman2@gmail.com>
2025-06-17 15:13:30 +0700

    fix the error
    Add ErrorViewModel and error handling Razor view
    
    Introduces a new `ErrorViewModel` class in the `VehicelDataApp.Models` namespace with properties for `RequestId` and `ShowRequestId`. Implements a Razor view to display error messages and the `RequestId` when available, along with guidance on enabling the Development environment for detailed error information. This enhances error handling and user feedback in the application.

---
+++ VehicelDataApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VehicelDataApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
